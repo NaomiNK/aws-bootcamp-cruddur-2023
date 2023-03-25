21bd010e234e4df3eb66846f063b6daeda6decc7
NaomiNK <naomikouadia@gmail.com>
2023-03-25 13:08:02 +0000

    commit activities

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -8,26 +8,44 @@ class Db:
   def _init_(self):  
     self.init_pool()
 
-def template(self,name):
-  template_path = os.path.join(app.root_path, 'db', 'sql',name+ '.sql')
-  with open(template_path, 'r') as f:
-    template_content = f.read()
-  return template_content
+  def template(self,*args):
+    pathing = list((app.root_path,'db','sql',) +args)
+    pathing[-1] = pathing[-1]+ ".sql"
+
+    template_path = os.path.join(*pathing)
+
+    green = '\033[92m'
+    no_color = '\033[0m'
+    print("\n")
+    print(f'{green} Load SQL Template-[(template_path)]---{no_color}')
+   
+    with open(template_path, 'r') as f:
+      template_content = f.read()
+    return template_content
 
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
   # we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
+  def print_params(self,params):
+    blue = '\033[94m'
+    no_color = '\033[0m'
+    print(f'{blue} SQL Params:{no_color}')
+    for key, value in params.items():
+      print(key, ":", value)
+
   def print_sql(self,tittle,sql):
     cyan = '\033[96m'
     no_color = '\033[0m'
-    print(f'{cyan}SQL STATEMENT-[(title)]---{no_color}')
-    print(sql + "\n")
-  def query_commit(self,sql,params):
+    print(f'{cyan} SQL STATEMENT-[(title)]---{no_color}')
+    print(sql)
+  def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql)
+
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
+
     try:
       with self.pool.connection() as coon:
        cur = conn.cursor()
@@ -37,29 +55,32 @@ def template(self,name):
        conn.commit()
        if is_returning_id:
          return returning_id
-    except Exception as error:
-      self.print_sql_err(err)
+    except Exception as err:
+      self.print_sql('err', sql)
   # when we want to return a json object
-  def query_array_json(self,sql):
-   print("SQL STATEMENT-[array]---")
-   print(sql + "\n")
-   wrapped_sql = self.query_wrap_array(sql)
-   with self.pool.connection() as conn:
-    with conn.cursor() as cur:
-      cur.execute(wrapped_sql)
-      json = cur.fetchone()
-      return json[0]
-  # when we want to return an array of json objects
-  def query_object_json(self,sql):
-    print("SQL STATEMENT-[object]---")  
-    print(sql + "\n")
-    wrapped_sql = self.query_wrap_object(sql)
+  def query_array_json(self,sql,params={}):
+    self.print_sql('array',sql)
+    wrapped_sql = self.query_wrap_array(sql)
+
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-       cur.execute(wrapped_sql)
-       json = cur.fetchone()
-       return json[0]
+        cur.execute(wrapped_sql)
+        json = cur.fetchone()
+        return json[0]
+  # when we want to return an array of json objects    
+  def query_object_json(self,sql,params={}):
+    self.print_sql('json', sql)
+    self.print_params(params)
+    wrapped_sql = self.query_wrap_object(sql)
 
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql,params={})
+        json = cur.fetchone()
+        if json == None:
+          "{}"
+        else:
+           return json[0]
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -67,14 +88,14 @@ def template(self,name):
     ) object_row);
     """
     return sql
-def query_wrap_array(self,template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
-def print_psycopg_exception(self,err):
+  def query_wrap_array(self,template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+  def print_psycopg_exception(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
 
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime, timedelta, timezone
 
 from lib.db import db
@@ -43,23 +42,25 @@ class CreateActivity:
       }   
     else:
       expires_at = (now + ttl_offset)
-      CreateActivity.create_activitity(user_handle,message,expires_at)
-      model['data'] = {
-        'uuid': uuid.uuid4(),
-        'display_name': 'Andrew Brown',
-        'handle':  user_handle,
-        'message': message,
-        'created_at': now.isoformat(),
-        'expires_at': (now + ttl_offset).isoformat()
-      }
+      uuid = CreateActivity.create_activitity(user_handle,message,expires_at)
+     
+      object_json = CreateActivity.query_object_activity(uuid)
+      model['data'] = object_json
     return model
 
   def create_activitity(handle, message, expires_at):
-    sql = db.template('create_activity')
-    uuid = db.query_commit (sql,{
+    sql = db.template('activities','create')
+    uuid = db.query_commit(sql,{
       'handle': handle,
       'message': message,
       'expires_at': expires_at
     })
-  #def query_object_activity():
+    return uuid
+  def query_object_activity(uuid):
+    sql = db.template('activites','object')
+    return db.query_object_json(sql,{
+      'uuid': uuid
+    })
+
+
 
